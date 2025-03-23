3736638ebaa6939bbdf7e44c533e862e28ce7d4b
vonedward <me@edward-vonschondorf.dev>
2025-03-23 11:38:19 -0700

    Add leadnode class and associated unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -3,10 +3,10 @@ class HTMLNode():
         self.tag = tag # A string representing the HTML tag name (e.g. "p", "a", "h1", etc.)
         self.value = value # A string representing the value of the HTML tag (e.g. the text inside a paragraph)
         self.children = children # A list of HTMLNode objects representing the children of this node
-        self.props = props # A dictionary of key-value pairs representing the attributes of the HTML tag. For example, a link (<a> tag) might have {"href": "https://www.google.com"}
+        self.props = props # A dictionary of key-value pairs representing the attributes of the HTML tag.
 
     def to_html(self):
-        raise NotImplementedError("Child classes will implement this method.")
+        raise NotImplementedError("child classes will implement this method")
     
     def props_to_html(self):
         if self.props:
@@ -20,3 +20,16 @@ class HTMLNode():
         props = self.props_to_html()
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {props})"
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        if not self.tag:
+            return f"{self.value}"
+        props = ""
+        if self.props:
+            props = " " + self.props_to_html()
+        return f"<{self.tag}{props}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,9 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode(None, "This is just text")
         output = "HTMLNode(None, This is just text, None, None)"
@@ -20,14 +20,34 @@ class TestTextNode(unittest.TestCase):
             "target": "_blank",
         }
         node = HTMLNode("a", "Link Text", None, props)
-        output = "HTMLNode(a, Link Text, None, href=\"https://www.google.com\" target=\"_blank\")"
+        output = 'HTMLNode(a, Link Text, None, href="https://www.google.com" target="_blank")'
         self.assertEqual(repr(node), output)
 
     def test_not_implemented_error(self):
         node = HTMLNode(None, "Test")
         with self.assertRaises(NotImplementedError) as context:
             node.to_html()
-        self.assertEqual(str(context.exception), "Child classes will implement this method.")
+        self.assertEqual(str(context.exception), "child classes will implement this method")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_props(self):
+        props = {
+            "href": "https://www.google.com",
+            "target": "_blank",
+        }
+        node = LeafNode("a", "Link Text", props)
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com" target="_blank">Link Text</a>')
+
+    def test_leaf_to_html_value_error(self):
+        node = LeafNode("b", None)
+        with self.assertRaises(ValueError) as context:
+            node.to_html()
+        self.assertEqual(str(context.exception), "All leaf nodes must have a value")
 
 if __name__ == "__main__":
     unittest.main()
