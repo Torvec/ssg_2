3c955c226ee08e55e76325abe2391217f9e1fc87
vonedward <me@edward-vonschondorf.dev>
2025-03-24 08:49:29 -0700

    Add copy static files functionality

--- src/main.py
+++ src/main.py
@@ -1,23 +1,12 @@
-from textnode import TextNode, TextType
+from copy_static import copy_static_files
 
-from htmlnode import HTMLNode
+
+source_dir = "./static"
+dest_dir = "./public"
 
 
 def main():
-    text_node = TextNode("Some Text Here", TextType.TEXT)
-    url_node = TextNode("A link to my site", TextType.LINK, "https://edward-vonschondorf.dev/" )
-    print(text_node)
-    print(url_node)
-    html_node = HTMLNode(None, "This is Text")
-    html_node_2 = HTMLNode("p", "This is paragraph text")
-    link_attributes = {
-        "href": "https://www.google.com",
-        "target": "_blank",
-    }
-    html_node_3 = HTMLNode("a", "This is a link", None, link_attributes)
-    print(html_node)
-    print(html_node_2)
-    print(html_node_3)
+   copy_static_files(source_dir, dest_dir)
 
 
 if __name__ == "__main__":
